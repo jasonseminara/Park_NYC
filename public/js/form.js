5379b8bb436c7223f826497204d14b0ace37cbe8
Ross Soloff <rsoloff@gmail.com>
2017-02-22 14:04:50 -0500

    work on zone search

--- public/js/form.js
+++ public/js/form.js
@@ -1,5 +1,10 @@
+$(document).ready(function() {
+  $.get('/zone_search', renderZone, 'json');
+  console.log('connected')
+});
 
-var test = function(){
-
-console.log('connected')
-};
+var renderZone = function(data) {
+  var zoneNumber = data.zone_number;
+  var price = data.price;
+  var timeLimit = data.time_limit;
+}
