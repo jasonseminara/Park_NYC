5b67d36d50967e94abd00490403515076b8d2d24
Ross Soloff <rsoloff@gmail.com>
2017-02-24 16:38:27 -0500

    form pretty much done

--- public/js/form.js
+++ public/js/form.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-  //$.get('/zone_search', renderZone, 'json');
   console.log('connected');
   $('#zone_number').blur(function(){
     const zone_number = $(this).val();
     $.get('/zones/' + zone_number, function(data) {
       var $price = $('<p>').html('Zone Price Per Hour: $' + data[0].price);
-      var $timeLimit = $('<p>').html('Zone Time Limit: ' + data[0].time_limit);
+      var $timeLimit = $('<p>').html('Zone Time Limit: ' + data[0].time_limit + ' Hours');
       $('.price').empty();
       $('.price').append($price);
       $('.time_limit').empty();
@@ -14,29 +13,36 @@ $(document).ready(function() {
       $('#time_limit').val(data[0].time_limit);
     });
   });
-  /*$('#duration').blur(function(){
-
-
-    const duration = $(this).val()
-    $.get('/zones/' + zone_number, function(data) {
-      var total = data[0].price * duration;
-      console.log(total);
-      var $totalPrice = $('<p>').html('Total Price: ' + $('#price'.val));
-      console.log($('#price'.val))
+  $('#duration').blur(function(){
+    const duration = $(this).val();
+    var $price = $('#price').val();
+    var $timeLimit = $('#time_limit').val();
+    if (duration <= $timeLimit) {
+      totalPrice = duration * $price;
+      var $totalPrice = $('<p>').html('Total Price: $' + totalPrice);
       $('.total_price').empty();
       $('.total_price').append($totalPrice);
+      $('.duration_error').empty();
+    } else {
+      $(this).val("");
+      var $errorMessage = $('<p>').html("You have exceed this zone's time limit. Please choose a shorter time.");
+      $('.duration_error').append($errorMessage);
+      $('.total_price').empty();
+    }
+  });
+  (function() {
+    $('form > div > input').keyup(function() {
+        var empty = false;
+        $('form > div > input').each(function() {
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+        if (empty) {
+            $('#purchase').attr('disabled', 'disabled');
+        } else {
+            $('#purchase').removeAttr('disabled');
+        }
     });
-  })*/
+  })()
 });
-
-/*var renderZone = function(data) {
-  var zoneNumber = data.zone_number;
-  var price = data.price;
-  var timeLimit = data.time_limit;
-  console.log('I got it');
-};
-*/
-/*var test = function(){
-
-console.log('connected')
-};*/
