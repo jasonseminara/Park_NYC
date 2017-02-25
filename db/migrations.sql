af5237b0427399d8a14919cda9735a4c3dfd296a
John Christie <omeara.christie@gmail.com>
2017-02-25 14:39:22 -0500

    Reorganize db

--- db/migrations.sql
+++ db/migrations.sql
@@ -2,32 +2,33 @@ DROP TABLE IF EXISTS zones;
 DROP TABLE IF EXISTS drivers;
 DROP TABLE IF EXISTS leases;
 DROP TABLE IF EXISTS driver;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR,
+  password VARCHAR,
+  payment_info VARCHAR
+);
 
 CREATE TABLE leases (
-  id SERIAL PRIMARY KEY,
+  id INTEGER references users (id),
   zone_number INTEGER,
   price DECIMAL,
   time_limit INTEGER,
   plate_state VARCHAR,
   plate_number VARCHAR(10),
-  duration INTEGER,
-  cgnumber INTEGER
-);
-
-CREATE TABLE zones (
-  id SERIAL PRIMARY KEY,
-  zone_number INTEGER,
-  price DECIMAL,
-  time_limit INTEGER
+  duration INTEGER
 );
 
 
-INSERT INTO leases (zone_number, price, time_limit, plate_state, plate_number, duration, cgnumber) VALUES
-  (1, 2.50, 3, 'NY', 45667, 1, 456),
-  (1, 2.50, 3, 'NJ', 567, 3, 567),
-  (2, 3.00, 1, 'NY', 788, 5, 981);
+INSERT INTO users (email, password, payment_info) VALUES
+  ('katie@ga', 'password1', 155676),
+  ('ross@ga', 'password2', 245457788),
+  ('john@ga', 'password3', 35656578),
+  ('jason@ga', 'password4', 234567);
 
-INSERT INTO zones (zone_number, price, time_limit) VALUES
-  (5, 1, 1),
-  (2, 2, 2),
-  (3, 3, 3);
+INSERT INTO leases (id, zone_number, price, time_limit, plate_state, plate_number, duration) VALUES
+  ( 1, 1, 2.50, 3, 'NY', 45667, 1),
+  ( 2, 1, 2.50, 3, 'NJ', 567, 3),
+  ( 3, 2, 3.00, 1, 'NY', 788, 5);
